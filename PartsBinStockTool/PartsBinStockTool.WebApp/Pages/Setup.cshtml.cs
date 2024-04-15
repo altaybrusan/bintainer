7bb14d1a8758ff00b1261b4e6778ecff886a153e
altay.brusan <altay.brusan@codemerx.com>
2024-04-15 11:54:10 +0300

    bugfix: unnecessary links removed

--- PartsBinStockTool/PartsBinStockTool.WebApp/Pages/Setup.cshtml.cs
+++ PartsBinStockTool/PartsBinStockTool.WebApp/Pages/Setup.cshtml.cs
@@ -1,14 +1,12 @@
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PartsBinStockTool.WebApp.Pages
 {
-    public class ComponentModel : PageModel
+    public class SetupModel : PageModel
     {
         public void OnGet()
         {
         }
-
     }
 }
