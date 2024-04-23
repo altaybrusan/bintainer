6a982c7323faff076cfc64a529333f10d0d9872f
altay.brusan <altay.brusan@codemerx.com>
2024-04-23 16:18:42 +0300

    order page created. The table could be used in other applications

---
+++ Bintainer/Bintainer.WebApp/Pages/Order.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bintainer.WebApp.Pages
+{
+    public class OrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
