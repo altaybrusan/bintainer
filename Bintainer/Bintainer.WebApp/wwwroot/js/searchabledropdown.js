6ad5a1afdcf3dbed965da52fa624257d527bac37
altay.brusan <altay.brusan@codemerx.com>
2024-04-23 10:30:28 +0300

    searchable dropdown completed

--- Bintainer/Bintainer.WebApp/wwwroot/js/searchabledropdown.js
+++ Bintainer/Bintainer.WebApp/wwwroot/js/searchabledropdown.js
@@ -1,47 +1,18 @@
-﻿const searchInput = document.getElementById('searchInput');
-const dropdownButton = document.getElementById("dropdownMenuButton1");
-
-const handleInput = () => {
-    const inputValue = searchInput.value;
-    const dataAttribute = searchInput.getAttribute('data-source');
-    const results = JSON.parse(dataAttribute);
-    const parentElement = document.querySelector(".dropdown-menu");
-    const elementsToRemove = document.querySelectorAll("li");
-    elementsToRemove.forEach(element => {
-        element.remove();
-    });
-    if (inputValue) {
-        const matchingWords = results.filter(word => word.toLowerCase().includes(inputValue.toLowerCase()));
-        populateList(matchingWords, parentElement, inputValue);
-    } else {
-        populateList(results, parentElement);
-    }
-}
-
-const populateList = (words, parentElement, highlightText = '') => {
-    words.forEach(word => {
-        const listItem = document.createElement("li");
-        const link = document.createElement("a");
-        link.classList.add("dropdown-item");
-        link.href = "#";
-        link.textContent = word;
-        link.addEventListener("click", function () {
-            dropdownButton.textContent = word;
-            dropdownButton.focus();
-            // If you have a bootstrap dropdown, you might want to update this to change how the display behaves
-            //dropdownButton.dataset.bsOriginalTitle = word; // update this if using Bootstrap tooltips or similar
-        });
-        listItem.appendChild(link);
-        parentElement.appendChild(listItem);
-    });
-
+﻿function populateList(words, parentElement, dropdownButton) {
     if (words.length === 0) {
-        const listItem = document.createElement('li');
-        listItem.textContent = "No items found";
-        listItem.classList.add('dropdown-item');
-        parentElement.appendChild(listItem);
+        parentElement.append('<li class="dropdown-item">No items found</li>');
+    } else {
+        words.forEach(function (word) {
+            var listItem = $('<li>');
+            var link = $('<a>').addClass('dropdown-item').attr('href', '#').text(word);
+
+            link.on('click', function () {
+                dropdownButton.text(word);
+                dropdownButton.focus();
+            });
+
+            listItem.append(link);
+            parentElement.append(listItem);
+        });
     }
 }
-
-// Initial population of the list
-handleInput();
