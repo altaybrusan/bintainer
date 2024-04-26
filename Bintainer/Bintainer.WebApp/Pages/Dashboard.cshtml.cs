76fb2c40694d7ace587967bcd1881c978d10828b
altay.brusan <altay.brusan@codemerx.com>
2024-04-26 09:20:17 +0300

    dashboard page added.

---
+++ Bintainer/Bintainer.WebApp/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bintainer.WebApp.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
