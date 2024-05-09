950ca570cdfd8f4e8799e9148e939254fa077238
altay.brusan <altay.brusan@codemerx.com>
2024-05-09 15:59:33 +0300

    bugfix: PartAttribute.sql Id is converted to Identity.

--- Bintainer/Bintainer.Db/dbo/Tables/PartAttribute.sql
+++ Bintainer/Bintainer.Db/dbo/Tables/PartAttribute.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[PartAttribute]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL CONSTRAINT PK_PartAttribute PRIMARY KEY IDENTITY(1,10001), 
     [Name] NCHAR(50) NULL, 
     [Value] NCHAR(150) NULL,     
 )
