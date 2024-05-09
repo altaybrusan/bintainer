530e2973feab7c048f723dfbce1a39d847d1e39d
altay.brusan <altay.brusan@codemerx.com>
2024-05-09 16:24:40 +0300

    bugfix: PartAttributeTemplate and PartAttribute updated. Id made Identity type.

--- Bintainer/Bintainer.Db/dbo/Tables/PartAttribute.sql
+++ Bintainer/Bintainer.Db/dbo/Tables/PartAttribute.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[PartAttribute]
 (
-	[Id] INT NOT NULL CONSTRAINT PK_PartAttribute PRIMARY KEY IDENTITY(1,10001), 
+	[Id] INT NOT NULL CONSTRAINT PK_PartAttribute PRIMARY KEY IDENTITY(10000,1), 
     [Name] NCHAR(50) NULL, 
     [Value] NCHAR(150) NULL,     
 )
