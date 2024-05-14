16d708257e5b12c045dcb9349561a16f2e16d31c
altay.brusan <altay.brusan@codemerx.com>
2024-05-14 09:49:34 +0300

    Inventory form post submit handler created.

--- Bintainer/Bintainer.WebApp/Pages/Dashboard/Inventory.cshtml.cs
+++ Bintainer/Bintainer.WebApp/Pages/Dashboard/Inventory.cshtml.cs
@@ -1,12 +1,47 @@
+using Bintainer.WebApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace Bintainer.WebApp.Pages.Dashboard
 {
+    public class InventoryViewModel 
+    {
+        [JsonPropertyName("sectionName")]
+        public string SectionName { get; set; }= string.Empty;
+        [JsonPropertyName("width")]
+        public int Width { get; set; }
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+        [JsonPropertyName("subSections")]
+        public int Subsections { get; set; }
+    };
     public class InventoryModel : PageModel
     {
+        SignInManager<IdentityUser> _SignInManager;
+        BintainerContext _dbContext;
+
+
+        public InventoryModel(SignInManager<IdentityUser> signInManager, BintainerContext dbContext)
+        {
+            _SignInManager = signInManager;
+            _dbContext = dbContext;
+
+        }
+
         public void OnGet()
         {
+        }
+        public void OnPostSubmitForm([FromBody] List<InventoryViewModel> sections) 
+        {
+            foreach (var item in sections)
+            {
+                _dbContext.InventorySections.Add(new InventorySection() { SectionName = item.SectionName, Width = item.Width, Height = item.Height });
+            }
+
+            _dbContext.SaveChanges();
         }
     }
 }
