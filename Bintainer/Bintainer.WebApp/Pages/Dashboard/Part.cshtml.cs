0afa25ae038e602c18719d8d371cfdcaff6b80fe
altay.brusan <altay.brusan@codemerx.com>
2024-05-23 14:04:46 +0300

    The Part table updated. The Package is loaded from database and autocomplete is updated.

--- Bintainer/Bintainer.WebApp/Pages/Dashboard/Part.cshtml.cs
+++ Bintainer/Bintainer.WebApp/Pages/Dashboard/Part.cshtml.cs
@@ -1,10 +1,19 @@
+using Bintainer.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bintainer.WebApp.Pages.Dashboard
 {
     public class PartModel : PageModel
     {
+        public List<string?> Packages { get; set; } = new List<string?>();
+        BintainerContext _dbcontext;
+		public PartModel(BintainerContext dbContext)
+        {
+			_dbcontext = dbContext;
+            Packages = _dbcontext.PartPackages.Select(p => p.Name).ToList();
+		}
 
         public void OnGet()
         {
