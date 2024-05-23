ac6352aac980e2b578c4cbd92f64f0a3e328d834
altay.brusan <altay.brusan@codemerx.com>
2024-05-23 16:49:51 +0300

    PartGroup and PartGroupAssociation tables updated in Db project.

--- Bintainer/Bintainer.Db/dbo/Tables/PartGroup.sql
+++ Bintainer/Bintainer.Db/dbo/Tables/PartGroup.sql
@@ -1,8 +1,5 @@
 ﻿CREATE TABLE [dbo].[PartGroup]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [PartId] INT NOT NULL, 
     [Name] NCHAR(10) NULL, 
-    CONSTRAINT [FK_PartGroup_Part] FOREIGN KEY ([PartId]) REFERENCES [Part]([Id])
-
 )
---
+++ Bintainer/Bintainer.Db/dbo/Tables/PartGroupAssociation.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PartGroupAssociation]
+(
+	[PartId] INT NOT NULL,
+    [GroupId] INT NOT NULL,
+    CONSTRAINT [PK_Part_PartGroup] PRIMARY KEY ([PartId], [GroupId]),
+    CONSTRAINT [FK_Part_PartGroup_Part] FOREIGN KEY ([PartId]) REFERENCES [Part]([Id]),
+    CONSTRAINT [FK_Part_PartGroup_PartGroup] FOREIGN KEY ([GroupId]) REFERENCES [PartGroup]([Id])
+)
