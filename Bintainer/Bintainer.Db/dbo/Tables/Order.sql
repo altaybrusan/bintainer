8acb8d6a41f4b90aaf15650b04b41a46f7961c75
altay.brusan <altay.bn@gmail.com>
2024-08-11 22:40:59 +0300

    Order Table updated. The PartId removed from Order.

--- Bintainer/Bintainer.Db/dbo/Tables/Order.sql
+++ Bintainer/Bintainer.Db/dbo/Tables/Order.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[Order]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [PartId] NCHAR(100) NOT NULL, 
-    [OrderNumber] NCHAR(100) NOT NULL, 
+    [OrderNumber] NCHAR(100) NULL DEFAULT ('default'), 
     [Qunatity] INT NULL DEFAULT 0,
     [Supplier] NCHAR(100) NOT NULL DEFAULT ('default'),
     [OrderDate] DATETIME NULL, 
