6c582a1a51829494abf566d3989ac33bbe479266
altay.brusan <altay.bn@gmail.com>
2024-09-21 22:09:46 +0300

    partattributetemplateassociation table added to sql project.

---
+++ Bintainer/Bintainer.Db/dbo/Tables/PartAttributeTemplateAssociation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PartAttributeTemplateAssociation]
+(
+	[PartId] INT NOT NULL,
+    [AttributeTemplateId] INT NOT NULL,
+    CONSTRAINT [PK_Part_PartAttributeTemplate] PRIMARY KEY ([PartId], [AttributeTemplateId]),
+    CONSTRAINT [FK_Part_PartAttributeTemplate_Part] FOREIGN KEY ([PartId]) REFERENCES [Part]([Id]),
+    CONSTRAINT [FK_Part_PartAttributeTemplate_PartAttributeTemplate] FOREIGN KEY ([AttributeTemplateId]) REFERENCES [PartAttributeTemplate]([Id])
+
+)
