48b36845da492787c203bc030f1f6f04ea273732
altay.brusan <altay.bn@gmail.com>
2024-09-29 07:46:00 +0300

    BinSubspace sql updated. the SubspaceIndex added to table.

--- Bintainer/Bintainer.Db/dbo/Tables/BinSubspace.sql
+++ Bintainer/Bintainer.Db/dbo/Tables/BinSubspace.sql
@@ -3,6 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(2000,1), 
     [Capacity] INT NULL DEFAULT 0, 
     [BinId] INT NULL, 
+    [SubspaceIndex] INT NULL, 
     [Label] NCHAR(100) NULL, 
     CONSTRAINT [FK_BinSubspace_Bin] FOREIGN KEY ([BinId]) REFERENCES [Bin]([Id])
 
