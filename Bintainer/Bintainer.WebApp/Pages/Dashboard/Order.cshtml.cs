3f43420ff929dabb74906e5e643b8efe1a92ffb8
altay.brusan <altay.brusan@marsathletic.com>
2024-10-10 22:49:19 +0300

    order table made dynamic

--- Bintainer/Bintainer.WebApp/Pages/Dashboard/Order.cshtml.cs
+++ Bintainer/Bintainer.WebApp/Pages/Dashboard/Order.cshtml.cs
@@ -1,3 +1,4 @@
+using Bintainer.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,18 @@ namespace Bintainer.WebApp.Pages.Dashboard
 {
     public class OrderModel : PageModel
     {
+        BintainerDbContext _dbcontext;
+        public List<Part> Part { get; set; } = new List<Part>();
+
+        public OrderModel(BintainerDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+            Part= _dbcontext.Parts.ToList();
+        }
+
         public void OnGet()
         {
+
         }
     }
 }
