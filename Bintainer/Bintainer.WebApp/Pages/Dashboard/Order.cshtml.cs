0b8197410a4d264934729b1889b39b3fa814b9b0
altay.brusan <altay.brusan@marsathletic.com>
2024-10-12 21:36:31 +0300

    Backaend of order page for registering order updated to track updates.

--- Bintainer/Bintainer.WebApp/Pages/Dashboard/Order.cshtml.cs
+++ Bintainer/Bintainer.WebApp/Pages/Dashboard/Order.cshtml.cs
@@ -3,6 +3,7 @@ using Bintainer.WebApp.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bintainer.WebApp.Pages.Dashboard
 {
@@ -27,20 +28,44 @@ namespace Bintainer.WebApp.Pages.Dashboard
             if (ModelState.IsValid) 
             {
                 var UserId = User.Claims.ToList().FirstOrDefault(c => c.Type.Contains("nameidentifier"))?.Value;
-                Order order = new Order();
-                order.OrderNumber = request.OrderNumber;
-                order.OrderDate = request.OrderDate;
-                order.HandOverDate = request.HandoverDate;
-                order.Supplier = request.Supplier;
-                order.UserId = UserId;
-                foreach (var item in request.Parts) 
+                Order? registeredOrder = _dbcontext.Orders.Include(o => o.OrderPartAssociations).FirstOrDefault(o => o.OrderNumber == request.OrderNumber);
+                
+                if(registeredOrder is null) 
                 {
-                    Part? part = _dbcontext.Parts.FirstOrDefault(c => c.Id == item.PartId);
-                    if (part is not null)
-                        order.Parts.Add(part);
+                    Order order = new Order();
+                    order.OrderNumber = request.OrderNumber;
+                    order.OrderDate = request.OrderDate;
+                    order.HandOverDate = request.HandoverDate;
+                    order.Supplier = request.Supplier;
+                    order.UserId = UserId;
+                    foreach (var item in request.Parts)
+                    {
+                        Part? part = _dbcontext.Parts.FirstOrDefault(c => c.Id == item.PartId);
+                        if (part is not null) 
+                        {
+                            OrderPartAssociation association = new OrderPartAssociation();
+                            association.PartId = part.Id;
+                            association.Qunatity= item.Quantity;
+                            order.OrderPartAssociations.Add(association);
+                        }
+                    }
+                    _dbcontext.Orders.Add(order);
+                    _dbcontext.SaveChanges(true);
                 }
-                _dbcontext.Orders.Add(order);
-                _dbcontext.SaveChanges(true);                
+                else 
+                {
+                    registeredOrder.OrderDate = request.OrderDate;
+                    registeredOrder.HandOverDate= request.HandoverDate;
+                    registeredOrder.Supplier = request.Supplier;
+                    foreach (var item  in registeredOrder.OrderPartAssociations) 
+                    {
+                        item.Qunatity = request.Parts.FirstOrDefault(p => p.PartId == item.PartId)?.Quantity;
+                    }
+                    _dbcontext.Orders.Update(registeredOrder);
+                    _dbcontext.SaveChanges(true);
+
+                }
+
             }
 
 
