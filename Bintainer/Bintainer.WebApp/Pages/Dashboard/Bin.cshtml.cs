506214ee1c13da05a4e5d103a5aa7dbb7cd165f2
altay.brusan <altay.brusan@marsathletic.com>
2024-10-16 04:59:46 +0300

    autocomplete sample added to bin page.

--- Bintainer/Bintainer.WebApp/Pages/Dashboard/Bin.cshtml.cs
+++ Bintainer/Bintainer.WebApp/Pages/Dashboard/Bin.cshtml.cs
@@ -1,3 +1,4 @@
+using Bintainer.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,13 @@ namespace Bintainer.WebApp.Pages.Dashboard
 {
     public class BinModel : PageModel
     {
+        public List<Part> Part { get; set; } = new List<Part>();
+
+        public BinModel(BintainerDbContext dbContext)
+        {
+            Part = dbContext.Parts.ToList();
+
+        }
         public void OnGet()
         {
         }
--- Bintainer/Bintainer.WebApp/wwwroot/js/autocomplete.js
+++ Bintainer/Bintainer.WebApp/wwwroot/js/autocomplete.js
@@ -1,19 +1,8 @@
-﻿function autocomplete(inp) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
+﻿function autocomplete(inp, options) {
     var currentFocus;
 
-    // Parse the data-source attribute as JSON
-    var autocompleteData = [];
-
-    // Check if data-source attribute exists and is not empty
-    if (inp.dataset.source) {
-        try {
-            autocompleteData = JSON.parse(inp.dataset.source);
-        } catch (error) {
-            console.error('Error parsing autocomplete data:', error);
-        }
-    }
+    // Set the autocompleteData from the injected options
+    var autocompleteData = options || [];
 
     /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function (e) {
@@ -23,32 +12,24 @@
         currentFocus = -1;
         createAutocompleteList(val);
     });
+
     /*execute a function presses a key on the keyboard:*/
     inp.addEventListener("keydown", function (e) {
-        var x = document.getElementById(this.id + "-autocomplete-list"); // Added "-" before "autocomplete-list"
+        var x = document.getElementById(this.id + "-autocomplete-list");
         if (x) x = x.getElementsByTagName("div");
         if (e.keyCode == 40) {
-            /*If the arrow DOWN key is pressed,
-            increase the currentFocus variable:*/
             currentFocus++;
-            /*and make the current item more visible:*/
             addActive(x);
         } else if (e.keyCode == 38) { //up
-            /*If the arrow UP key is pressed,
-            decrease the currentFocus variable:*/
             currentFocus--;
-            /*and make the current item more visible:*/
             addActive(x);
         } else if (e.keyCode == 13) {
-            /*If the ENTER key is pressed, prevent the form from being submitted,*/
             e.preventDefault();
-            if (currentFocus > -1) {
-                /*and simulate a click on the "active" item:*/
-                if (x) x[currentFocus].click();
+            if (currentFocus > -1 && x) {
+                x[currentFocus].click();
             }
         }
     });
-
 
     function createAutocompleteList(val) {
         var a, b, i;
@@ -56,6 +37,7 @@
         a.setAttribute("id", inp.id + "-autocomplete-list");
         a.setAttribute("class", "autocomplete-items");
         inp.parentNode.appendChild(a);
+
         for (i = 0; i < autocompleteData.length; i++) {
             if (autocompleteData[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
                 b = document.createElement("DIV");
@@ -70,25 +52,22 @@
             }
         }
     }
+
     function addActive(x) {
-        /*a function to classify an item as "active":*/
         if (!x) return false;
-        /*start by removing the "active" class on all items:*/
         removeActive(x);
         if (currentFocus >= x.length) currentFocus = 0;
         if (currentFocus < 0) currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
         x[currentFocus].classList.add("autocomplete-active");
     }
+
     function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
         for (var i = 0; i < x.length; i++) {
             x[i].classList.remove("autocomplete-active");
         }
     }
+
     function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-        except the one passed as an argument:*/
         var x = document.getElementsByClassName("autocomplete-items");
         for (var i = 0; i < x.length; i++) {
             if (elmnt != x[i] && elmnt != inp) {
@@ -96,15 +75,19 @@
             }
         }
     }
-    /*execute a function when someone clicks in the document:*/
+
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
     });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var autocompleteInputs = document.querySelectorAll(".autocomplete-input");
+    // Get all autocomplete inputs on the page
+    var autocompleteInputs = document.querySelectorAll("[data-autocomplete-options]");
+
+    // For each input, initialize the autocomplete with the corresponding options
     autocompleteInputs.forEach(function (input) {
-        autocomplete(input);
+        var options = JSON.parse(document.getElementById(input.dataset.autocompleteOptions).textContent);
+        autocomplete(input, options);
     });
 });
