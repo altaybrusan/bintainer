f5eabe8e0a67a029dd908be57e312f0e63b2bf4c
altay.brusan <altay.brusan@marsathletic.com>
2024-11-01 22:12:02 +0300

    global constants added.

---
+++ Bintainer/Bintainer.SharedResources/Const/GlobalConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bintainer.SharedResources.Const
+{
+    public class GlobalConstants
+    {
+        public const int MaxSectionWidth = 100;
+        public const int MinSectionWidth = 1;       
+        public const int MaxSectionHeight = 20;       
+        public const int MinSectionHeight = 2;
+        public const int MaxSubspace = 3;
+        public const int MinSubspace = 1;
+    }
+}
